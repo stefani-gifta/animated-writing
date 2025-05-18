9be23c6c9938ee8444fe781e613fa41246e7cd30
gifta <152134013+stefani-gifta@users.noreply.github.com>
2025-05-18 23:42:18 +0700

    Change .txt to .js

---
+++ text/stories.js
@@ -0,0 +1,159 @@
+const stories = {
+
+story1: `Under these celebrating cheers and lights, all the memories slowly hit me. I’m standing by the quietest corner, but not quite in it—it’s already occupied. He is already standing there.
+
+“Are you leaving tomorrow?”
+
+He’s asking if I’m going back to my hometown after this New Year celebration. I looked at him, but he kept staring at the bright light cities across this concrete railing.
+
+“Uh, yeah, I guess. You?”
+
+I guess it was just formality of asking back, but I actually didn’t care—or, let me revise that—I didn’t want him to think that I still care.
+
+He answered no, and we fell into silence again. I hate that this isn’t awkward. We’ve gotten too comfortable with each other’s presence. Maybe that sounds good, but it confuses me about where to draw the line.
+
+Because when it comes to him, I just want to keep it short.
+
+There was a time when I, being the dumb stupid self that I was, fell for him. Not for his looks, or his personality, but for his intelligence. The way his mind thinks. But I fell out of him because of that same mind. He was simply too selfish and practical.
+
+“Hey, you two!” From behind us, a high, shrieking loud voice broke my divine deep thinking. “Come count or I’ll kick you out for being boring!”
+
+I laughed and ran inside. He just scoffed and walked slowly, keeping it cool. Like he always do.
+
+“…3, …2, …1! Happy New Year!”
+
+Glasses clinking and I took a shot. What a night this has been. Fine, maybe one more. Or another. And another …
+
+*
+
+Fuck. I woke up next to him. I must have drank too much. Again. This is getting predictable. This game with him which I never win.
+
+I always say I want it short and simple, like a goodbye text. But we keep bumping into each other, and the bumping becomes an accident, and my heart goes to ICU for that in the end, while his getting colder and colder.
+
+We once dreamt of having kids. Yes, plural. Two would be fun, but three would feel like having a cute trio in the house. We couldn’t decide. But look at us now. Who thought that one child would ruin us?
+
+*
+
+I thought it would be a great idea to visit her cemetery after his dream last night. “Don’t you ever think that she probably needs both parents to be there sometimes?” I expressed my opinions, to which he just gave in. We never argue except for the day I wanted the divorce.
+
+But it suddenly rained.
+
+“Is she crying from above?” That wasn’t asked to anybody.
+
+“That’s what the therapist meant when she said more positive imagination, huh?”
+
+I rolled my eyes to his remark.
+
+*
+
+I know that in a year, or maybe two, or maybe ten, this would all be okay. Maybe I’ll find somebody new, and he will too. Maybe we’ll both have kids again with our next partners.
+
+But right now, my young mistake is weighing heavily.
+
+And I’m still figuring out how to end things properly.`,
+
+story2: `What happened to us? Well, let me tell you the story.
+
+*
+
+"Our streak pet is going to be purple!" It's 12 AM and I was about to turn my phone off when the SickTok notification popped up.
+
+That means we have succesfully texted each other everyday on SickTok for 3 years without fail. There's no such thing as streak recovery in this app, so we never went a day without sending a message to one another.
+
+I immediately opened her text. "Hell yeah!" I texted her back to activate our 1095th streak and went to sleep without forgetting to say good night.
+
+At least, I tried. My eyes wouldn't close for more than 5 minutes. My thoughts kept on rushing.
+
+"Should I tell her about my feelings?"
+
+"What will happen to our friendship?"
+
+"What about our 3 years streak on SickTok?"
+
+I had an Alibaba Cloud exam the next day. I needed to sleep. But how?
+
+Suddenly, my mind recalled a song lyric. "If I tell you all my feelings, would you believe me?"
+
+That's Ali Gatie's song, "What If I Told You That I Love You". That lyric made it less about me and more about her. If I told her, would she believe me? Because even after 2 years, I still couldn't believe my own feelings.
+
+*
+
+"I love you. So much." Her text 5 years ago. I remember that vividly.
+
+It was weird that she didn't shorten her words as usual, especially with the "so much". But I treated it like any other "Ily" messages me and my girlfriends always text each other. "Yk ily 2," I answered.
+
+"No, I meant, as more than friends," she texted again after a minute.
+
+I got hit with the realization that she just came out ....
+
+"It's u. U're the 1 who makes me feel sure about it," she texted.
+
+*
+
+I was only 14. I had no idea I was going to get my heart broken by many immature boys after that. If only I could see the future, I would have returned her feelings back then. But life doesn't work that way.
+
+So for the next 2 years, we stopped being friends as she dealt with her sexuality and my rejection. A little over 3 years ago, however, her video passed my For You page, and I started following her account.
+
+A New Year party had resulted in us bumping around that time. It felt like fate.
+
+You know what, maybe it <i>was</i> fate. I think I'm going to send her the lyrics with Spotify. After all, our friendship survived the pandemic. Maybe we really do have a strong chemistry. So I shared the lyric. And immediately turned my phone off again. Too scared to face her reply.
+
+I woke up to her good morning text as usual, but she paid no attention to my act of courage last night. I wanted to mention it, but I was scared of sounding desperate. Well, maybe she forgot to reply. I shrugged it off.
+
+*
+
+A week had passed by, and my disappointment had too.
+
+Ding.
+
+A notification popped. She just posted a video on SickTok! God, yes. I've missed seeing her face. Her face, so sweet like cupcake. Hmm!
+
+I stared at the video. I replayed it. I zoomed it.
+
+And slowly, tears filled my eyes. Rained down as I face the bitter end of our 3 years friendship—the one I thought was going to bloom last week.
+
+*
+
+"U shud've told me. I thot we're friends?!" I text her with rage.
+
+"Wait, what is this about?"
+
+"Oh, fuck you. Yk exactly what this is abt. Who's she?"
+
+"Alr, would u calm down?"
+
+"Go to hell."
+
+"Hey! I'm in a healthy n happy relationship. I'm about to have kids. The least u can do is be happy for me!"
+
+"How could u not tell me?"
+
+"It was a secret."
+
+"U've made videos with her before!"
+
+"Uhh, yeah, as friends. It hasn't even been a month, calm down."
+
+"Oh .... God, what about those good mornin n good nite texts?"
+
+"Yk Ily so much .... But I've gotten past us some years ago."
+
+There. Those words again. But an "Ily" doesn't hit as deep as an "I love you". What a fool I was, hoping she was still waiting for me.
+
+"We're friends, so why shudn't I send gm n gn?" She sent a message again.
+
+"Because we're lesbians .... And we have a history ...." But I answered that only in my mind .... In my heart ....
+
+I called the streak off. She was begging me not to, saying she wanted to keep it up for 2 more years so we can have a red streak. To which I replied, "Well, do that with your future wife, honey."
+
+We will never be able to be friends anymore. Not when I have strong unreciprocated feelings like this. Feelings that should have been felt 5 years ago.
+
+Us being this close might jeopardize her relationship too. I don't want her to break up because of me; not even <i>for</i> me. I guess I love her so much too; that I let her go when I should.
+
+*
+
+Ah .... Now that I look back, it starts to click. Of course she did not reply to my lyric because she was already in a relationship.
+
+Now I get it. She sent me the sweet texts to make me stay, without ever meaning them, without a care about how it would affect me. I'd like to think it was right for me to walk away before she could permanently scar me.`
+
+}
